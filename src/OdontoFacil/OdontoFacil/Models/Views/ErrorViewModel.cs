727dfa75f15c30b0565d0e1e51eb5697d4ba30b5
Jean Karlo <78477974+karlojean@users.noreply.github.com>
2025-04-19 20:23:29 -0300

    Adicionar projeto dotnet (#23)
    * adicionar projeto dotnet base
    
    * atualizar gitignore

---
+++ src/OdontoFacil/OdontoFacil/Models/Views/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OdontoFacil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
