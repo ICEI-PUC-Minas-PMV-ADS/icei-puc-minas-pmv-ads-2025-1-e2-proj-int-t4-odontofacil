057e026f3ea10f4220eebd17ffca85bead277e52
Heitor Oliveira <heitor.flavio@sga.pucminas.br>
2025-05-08 22:26:37 -0300

    adicionando tela 1 de agendamentos

---
+++ src/OdontoFacil/OdontoFacil/Controllers/AgendamentoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdontoFacil.Controllers
+{
+    public class AgendamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
