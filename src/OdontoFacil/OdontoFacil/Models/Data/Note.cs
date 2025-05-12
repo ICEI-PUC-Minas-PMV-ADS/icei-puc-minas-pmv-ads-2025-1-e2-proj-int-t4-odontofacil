22851ae4162a5e9b6bb3315a71ebaa5340c2d73a
Lucas Mamédio <luccasm.v@hotmail.com>
2025-05-11 22:44:58 -0300

    Telas de pacientes do dia 2

---
+++ src/OdontoFacil/OdontoFacil/Models/Data/Note.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OdontoFacil.Models.Data;
+
+[Table(name: "Anotacoes")]
+public partial class Note
+{
+    [Key]
+    [Column(name: "id")]
+   
+    public string Id { get; set; } = null!;
+
+    [Column(name: "nome")]
+    public string PatientNote { get; set; } = null!;
+
+    [Column(name: "conteudo")]
+    public string ContentNote { get; set; } = null!;
+
+    [Column(name: "data_criacao")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [Column(name: "id_paciente")]
+    [ForeignKey(nameof(Patient))]
+    public string PatientId { get; set; }
+
+    [Column(name: "id_dentista")]
+    [ForeignKey(nameof(Dentist))]
+    public string DentistId { get; set; }
+
+    public Patient Patient { get; set; }
+
+    public Dentist Dentist { get; set; }
+
+}
---
+++ src/OdontoFacil/OdontoFacil/Models/Views/PatientViewModel.cs
@@ -0,0 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using OdontoFacil.Models.Data;
+
+namespace OdontoFacil.Models.Views;
+
