b76e9230d955046fec0f538939daa566b2838619
Rodrigo Rodrigues <git@rdrd.dev>
2025-05-24 21:04:05 +0000

    feat: rascunho

--- src/OdontoFacil/OdontoFacil/Models/Views/ScheduleAppointmentViewModel.cs
+++ src/OdontoFacil/OdontoFacil/Models/Views/ScheduleAppointmentViewModel.cs
@@ -8,11 +8,15 @@ public class ScheduleAppointmentViewModel
     [Required(ErrorMessage = "O campo dentista é obrigatório.")]
     public string DentistId { get; set; } = null!;
 
-    [Display(Name = "Data")]
+    [Display(Name = "Datas disponíveis")]
     [Required(ErrorMessage = "O campo data é obrigatório.")]
     public string Date { get; set; } = null!;
 
-    [Display(Name = "Hora")]
+    [Display(Name = "Horários disponíveis")]
     [Required(ErrorMessage = "O campo hora é obrigatório.")]
     public string Time { get; set; } = null!;
+
+    [Display(Name = "Paciente")]
+    [Required(ErrorMessage = "O campo paciente é obrigatório.")]
+    public string UserId { get; set; } = null!;
 }
