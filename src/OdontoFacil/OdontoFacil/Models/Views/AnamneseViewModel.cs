ebad2042415b49102c191da79da206eebb27c4a4
Sergiotesh <158783134+Sergiotesh@users.noreply.github.com>
2025-06-08 12:04:29 -0300

    Tela Anaminese

---
+++ src/OdontoFacil/OdontoFacil/Models/Views/AnamneseViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace OdontoFacil.Models.Views
+{
+    public class AnamneseViewModel
+    {
+        public string PacienteId { get; set; }
+
+        public List<PerguntaResposta> Perguntas { get; set; }
+
+        public class PerguntaResposta
+        {
+            public string? Id { get; set; }
+            public string PerguntaId { get; set; }
+            public string Descricao { get; set; }
+            public bool? Resposta { get; set; }
+            public string? RespostaTextual { get; set; }
+        }
+    }
+}
